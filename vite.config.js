4a7545fa545a7a7fc38f15f81fce90ff69f1e5c0
Ollie <wang.yichi@northeastern.edu>
2024-12-16 13:30:04 -0600

    Adds stuff

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
+import postcssFlexbugsFixes from "postcss-flexbugs-fixes";
+import postcssPresetEnv from "postcss-preset-env";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +11,13 @@ export default defineConfig({
       plugins: [
         autoprefixer({
           grid: "autoplace", // 強制支援舊版 CSS Grid
+        }),
+        postcssFlexbugsFixes(), // 修復 Flexbox 和 Grid 的 bug
+        postcssPresetEnv({
+          stage: 3, // 啟用現代 CSS 功能並兼容舊版
+          features: {
+            "align-content": false, // 處理 align-items/align-self 問題
+          },
         }),
       ],
     },
