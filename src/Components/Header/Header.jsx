02e1aeb6f6712adfe6b6da71ebaaef747aa05a4a
Ollie <wang.yichi@northeastern.edu>
2024-12-16 14:39:33 -0600

    Adds modification

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -16,6 +16,16 @@ function Header() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
+    // 檢測瀏覽器並添加 "safari" class 到 <html>
+    if (
+      navigator.userAgent.includes("Safari") &&
+      !navigator.userAgent.includes("Chrome")
+    ) {
+      document.documentElement.classList.add("safari");
+    }
+  }, []);
+
+  useEffect(() => {
     function handlePageLoad() {
       setPage(document.location.pathname);
     }
